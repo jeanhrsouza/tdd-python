7cf31cf4c402fdc9c5757de92809ded01a47f11b
Jean Souza <jeanhrsouza@hotmail.com>
2021-01-16 23:57:57 -0300

    Comecando com testes de unidade

---
+++ src/leilao/test_dominio.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from src.leilao.dominio import Usuario, Lance, Leilao, Avaliador
+
+
+class TestAvaliador(TestCase):
+
+    def test_avalia(self):
+        # criando usuários
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        # propondo lances
+        lance_do_yuri = Lance(yuri, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+
+        # criando leilao
+        leilao = Leilao('Celular')
+
+        # adicionando valores de lances no leilao
+        leilao.lances.append(lance_do_yuri)
+        leilao.lances.append(lance_do_gui)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
