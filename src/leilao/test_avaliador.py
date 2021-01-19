021e1120bd07100d0b9f08b473b4685a5bbb0aad
Jean Souza <jeanhrsouza@hotmail.com>
2021-01-18 21:05:32 -0300

    Criando um novo teste

--- src/leilao/test_avaliador.py
+++ src/leilao/test_avaliador.py
@@ -29,3 +29,29 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+
+    def test_avalia2(self):
+        # criando usuários
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        # propondo lances
+        lance_do_yuri = Lance(yuri, 100.0)
+        lance_do_gui = Lance(gui, 150.0)
+
+        # criando leilao
+        leilao = Leilao('Celular')
+
+        # adicionando valores de lances no leilao
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_yuri)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
