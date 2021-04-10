4275e1cde17088cc62fc6e7b59f09c5e1c59c6c6
Jean Souza <jeanhrsouza@hotmail.com>
2021-04-10 14:30:11 -0300

    Duas novas regras e novos testes

--- src/leilao/test_leilao.py
+++ src/leilao/test_leilao.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from src.leilao.dominio import Usuario, Lance, Leilao
 
 
-class TestAvaliador(TestCase):
+class TestLeilao(TestCase):
 
     def setUp(self):
         # criando usuários
@@ -19,9 +19,6 @@ class TestAvaliador(TestCase):
         # adicionando valores de lances no leilao
         self.leilao.propoe(lance_do_yuri)
         self.leilao.propoe(self.lance_do_gui)
-
-
-
 
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
@@ -39,7 +36,6 @@ class TestAvaliador(TestCase):
         self.leilao.propoe(lance_do_yuri)
 
 
-
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
 
@@ -49,8 +45,6 @@ class TestAvaliador(TestCase):
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
         self.leilao.propoe(self.lance_do_gui)
-
-
 
         self.assertEqual(150.0, self.leilao.menor_lance)
         self.assertEqual(150.0, self.leilao.maior_lance)
@@ -64,7 +58,6 @@ class TestAvaliador(TestCase):
 
         lance_do_vini = Lance(vini, 200.0)
 
-
         # adicionando valores de lances no leilao
         self.leilao.propoe(self.lance_do_gui)
         self.leilao.propoe(lance_do_yuri)
@@ -75,4 +68,38 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+
+    # Se o leilão não tiver lances, deve permitir propor um lance
+    def test_deve_permitir_propor_um_lance_caso_o_leilao_nao_tenha_lances(self):
+        self.leilao.propoe(self.lance_do_gui)
+
+        quantidade_de_lances_recebido = len(self.leilao.lances)
+        self.assertEqual(1, quantidade_de_lances_recebido)
+
+    # Se o último usuário for diferente, deve permitir propor o lance
+    def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
+        yuri = Usuario('Yuri')
+        lance_do_yuri = Lance(yuri, 200.0)
+
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
+
+        quantidade_de_lances_recebido = len(self.leilao.lances)
+
+        self.assertEqual(2, quantidade_de_lances_recebido)
+
+
+    # Se o último usuário for o mesmo, não deve permitir propor o lance
+    def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
+        lance_do_gui200 = Lance(self.gui, 200)
+
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_gui200)
+
+        quantidade_de_lance_recebido = len(self.leilao.lances)
+
+        self.assertEqual(1, quantidade_de_lance_recebido)
+
+
 
