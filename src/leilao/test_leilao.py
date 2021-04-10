90d66acf38d067e62b01dab748183b9392417eb2
Jean Souza <jeanhrsouza@hotmail.com>
2021-04-10 15:11:54 -0300

    Testando exceções e TDD

--- src/leilao/test_leilao.py
+++ src/leilao/test_leilao.py
@@ -94,12 +94,9 @@ class TestLeilao(TestCase):
     def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
         lance_do_gui200 = Lance(self.gui, 200)
 
-        self.leilao.propoe(self.lance_do_gui)
-        self.leilao.propoe(lance_do_gui200)
-
-        quantidade_de_lance_recebido = len(self.leilao.lances)
-
-        self.assertEqual(1, quantidade_de_lance_recebido)
+        with self.assertRaises(ValueError):
+            self.leilao.propoe(self.lance_do_gui)
+            self.leilao.propoe(lance_do_gui200)
 
 
 
