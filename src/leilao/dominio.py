9785a85e693a3bf42bba7e8362ebeaa2051e9c20
Jean Souza <jeanhrsouza@hotmail.com>
2021-04-11 21:14:51 -0300

    baby steps

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -26,7 +26,8 @@ class Leilao:
         self.menor_lance = sys.float_info.max
 
     def propoe(self, lance: Lance):
-        if not self.__lances or self.__lances[-1].usuario != lance.usuario:
+        # se não houver lances OU o último usuário não seja o mesmo E o valor do lance seja maior que o valor do ultimo
+        if not self.__lances or self.__lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor:
             if lance.valor > self.maior_lance:
                 self.maior_lance = lance.valor
             if lance.valor < self.menor_lance:
@@ -34,7 +35,7 @@ class Leilao:
 
             self.__lances.append(lance)
         else:
-            raise ValueError('O mesmo usuário não pode propor dois lances seguidos')
+            raise ValueError('Erro ao propor lance')
 
     @property
     def lances(self):
--- src/leilao/test_leilao.py
+++ src/leilao/test_leilao.py
@@ -27,20 +27,14 @@ class TestLeilao(TestCase):
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
 
-    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionado_em_ordem_decrescente(self):
-        yuri = Usuario('Yuri')
-        lance_do_yuri = Lance(yuri, 100.0)
+    def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
+        with self.assertRaises(ValueError):
+            yuri = Usuario('Yuri')
+            lance_do_yuri = Lance(yuri, 100.0)
 
-        # adicionando valores de lances no leilao
-        self.leilao.propoe(self.lance_do_gui)
-        self.leilao.propoe(lance_do_yuri)
-
-
-        menor_valor_esperado = 100.0
-        maior_valor_esperado = 150.0
-
-        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
-        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+            # adicionando valores de lances no leilao
+            self.leilao.propoe(self.lance_do_gui)
+            self.leilao.propoe(lance_do_yuri)
 
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
@@ -55,12 +49,11 @@ class TestLeilao(TestCase):
         lance_do_yuri = Lance(yuri, 100.0)
 
         vini = Usuario('Vini')
-
         lance_do_vini = Lance(vini, 200.0)
 
         # adicionando valores de lances no leilao
-        self.leilao.propoe(self.lance_do_gui)
         self.leilao.propoe(lance_do_yuri)
+        self.leilao.propoe(self.lance_do_gui)
         self.leilao.propoe(lance_do_vini)
 
         menor_valor_esperado = 100.0
